ace731dd096ef2b338cd8f1dea543c8448fba9b6
Xavier Shay <contact@xaviershay.com>
2019-01-31 17:40:39 -0800

    Sketching out types.

--- api/app/Main.hs
+++ api/app/Main.hs
@@ -2,9 +2,16 @@ module Main where
 
 import Network.Wai.Handler.Warp (run)
 import Hanabi
+import Hanabi.Types
+import Hanabi.Api
+import Data.Aeson
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import           Control.Monad.IO.Class               (liftIO)
 
 main :: IO ()
 main = do
   let port = 8080
   state <- mkState
+  now  <- liftIO getCurrentTime
+  --putStrLn . show $ (encode . apply (Choice (PlayerId 123) (ChoicePlayCard (CardId 1))) $ mkGame now)
   run port (app state)
--- api/src/Hanabi/Types.hs
+++ api/src/Hanabi/Types.hs
@@ -5,21 +5,80 @@ module Hanabi.Types where
 
 import GHC.Generics
 
-import           Control.Lens
-import           Data.Time.Clock     (UTCTime)
+import Control.Lens hiding (Choice)
 import Data.Aeson
+import Data.Time.Clock (UTCTime)
+import qualified Data.HashMap.Strict as M
+import Data.Hashable
+
+newtype PlayerId = PlayerId Int deriving (Show, Eq, Generic)
+newtype CardId = CardId Int deriving (Show, Eq, Generic)
+
+data Color = Red | Blue | Green | Yellow | White deriving (Show, Enum, Generic)
+data Location = Hand PlayerId | Deck | Board | Discard deriving (Show, Generic)
+
+data Visibility = Hidden | HiddenFrom PlayerId | Visible deriving (Show, Generic)
+
+data Card = Card
+  { _cardId :: CardId
+  , _cardRank :: Int
+  , _cardColor :: Color
+  , _cardLocation :: Location
+  , _cardVisibility :: Visibility
+  } deriving (Show, Generic)
+
+data PlayerChoice =
+  ChoicePlayCard CardId
+  deriving (Show)
+
+data Choice = Choice PlayerId PlayerChoice deriving (Show)
 
 data Game = Game
   { _gameVersion :: Integer
   , _gameModified :: UTCTime
+  , _gameCards :: M.HashMap CardId Card
   } deriving (Generic)
 
 makeLenses ''Game
+makeLenses ''Card
 
 mkGame :: UTCTime -> Game
 mkGame now = Game
   { _gameVersion = 1
   , _gameModified = now
+  , _gameCards = M.fromList [(CardId 1, mkFakeCard)] --mempty
   }
 
+mkFakeCard = Card
+  { _cardId = CardId 1
+  , _cardRank = 2
+  , _cardColor = Red
+  , _cardLocation = Hand (PlayerId 123)
+  , _cardVisibility = Visible
+  }
+
+apply :: Choice -> Game -> Game
+apply (Choice pid (ChoicePlayCard cid)) =
+  over
+    (gameCards . at cid . _Just)
+    (\card ->
+        case view cardLocation card of
+          Hand cardPlayerId -> if cardPlayerId == pid then
+                                 set cardLocation Board card
+                               else
+                                 card
+          _ -> card
+    )
+
+apply _ = error "unimplemented"
+
+
+instance ToJSON CardId
+instance ToJSONKey CardId
+instance ToJSON Visibility
+instance ToJSON Location
+instance ToJSON PlayerId
+instance ToJSON Color
+instance ToJSON Card
 instance ToJSON Game
+instance Hashable CardId
