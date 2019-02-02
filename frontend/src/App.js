042c77a68fff953087230282a21413abe177a91e
Xavier Shay <contact@xaviershay.com>
2019-02-02 07:46:00 -0800

    Mocking up some cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,58 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <p>Hello</p>
+    return <div class='board' style={{position:"relative",width:"500px", height: "500px"}}>
+      <div class='card red' style={{left: 0, top: 5}}>
+        <div class='icons'>
+          <span>
+          <i class="fas fa-heart"></i>
+          <i class="fas fa-heart"></i>
+          <i class="fas fa-heart"></i>
+          <i class="fas fa-heart"></i>
+          <i class="fas fa-heart"></i>
+      </span>
+        </div>
+        <div>5</div>
+      </div>
+      <div class='card green' style={{left: 80, top: 5}}>
+        <div class='icons'>
+          <span>
+          <i class="fas fa-leaf"></i>
+          <i class="fas fa-leaf"></i>
+          </span>
+        </div>
+        <div>2</div>
+      </div>
+      <div class='card yellow' style={{left: 160, top: 5}}>
+        <div class='icons'>
+          <span>
+            <i class="fas fa-bolt"></i>
+            <i class="fas fa-bolt"></i>
+            <i class="fas fa-bolt"></i>
+          </span>
+        </div>
+        <div>3</div>
+      </div>
+      <div class='card blue' style={{left: 240, top: 5}}>
+        <div class='icons'>
+          <span>
+          <i class="fas fa-tint"></i>
+          </span>
+        </div>
+        <div>1</div>
+      </div>
+      <div class='card white' style={{left: 320, top: 5}}>
+        <div class='icons'>
+          <span>
+            <i class="fas fa-balance-scale"></i>
+            <i class="fas fa-balance-scale"></i>
+            <i class="fas fa-balance-scale"></i>
+            <i class="fas fa-balance-scale"></i>
+          </span>
+        </div>
+        <div>4</div>
+      </div>
+    </div>
   }
 
   componentDidMount() {
