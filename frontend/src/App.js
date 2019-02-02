425c56da6b86a84efc563bf4c0fec045d1cb2b37
Xavier Shay <contact@xaviershay.com>
2019-02-02 08:24:52 -0800

    More UI sketching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <i class="fas fa-heart"></i>
       </span>
         </div>
-        <div>5</div>
+        <div class='number'>5</div>
       </div>
       <div class='card green' style={{left: 80, top: 5}}>
         <div class='icons'>
@@ -23,7 +23,7 @@ class App extends Component {
           <i class="fas fa-leaf"></i>
           </span>
         </div>
-        <div>2</div>
+        <div class='number'>2</div>
       </div>
       <div class='card yellow' style={{left: 160, top: 5}}>
         <div class='icons'>
@@ -33,7 +33,7 @@ class App extends Component {
             <i class="fas fa-bolt"></i>
           </span>
         </div>
-        <div>3</div>
+        <div class='number'>3</div>
       </div>
       <div class='card blue' style={{left: 240, top: 5}}>
         <div class='icons'>
@@ -41,7 +41,7 @@ class App extends Component {
           <i class="fas fa-tint"></i>
           </span>
         </div>
-        <div>1</div>
+        <div class='number'>1</div>
       </div>
       <div class='card white' style={{left: 320, top: 5}}>
         <div class='icons'>
@@ -52,7 +52,43 @@ class App extends Component {
             <i class="fas fa-balance-scale"></i>
           </span>
         </div>
-        <div>4</div>
+        <div class='number'>4</div>
+      </div>
+      <div class='card white' style={{left: 0, top: 115}}>
+        <div style={{opacity: 0.3}}>?</div>
+      </div>
+      <div class='card white' style={{left: 80, top: 115}}>
+        <div style={{opacity: 0.5}}><i class='fas fa-balance-scale'></i></div>
+      </div>
+      <div class='card green' style={{left: 240, top: 115}}>
+        <div style={{opacity: 0.5}}><i class='fas fa-leaf'></i></div>
+      </div>
+      <div class='card white' style={{left: 160, top: 115}}>
+        <div class='icons' style={{opacity: 0.3}}>
+          <span>
+            <i class="fas fa-question"></i>
+            <i class="fas fa-question"></i>
+      <br/>
+            <i class="fas fa-question"></i>
+            <i class="fas fa-question"></i>
+          </span>
+        </div>
+        <div style={{opacity: 0.3}} class='number'>4</div>
+      </div>
+      <div class='card white' style={{left: 320, top: 115}}>
+        <div class='icons' style={{opacity: 0.5}}>
+          <span>
+            <i class="fas fa-heart"></i>
+            <i class="fas fa-leaf" style={{visibility: "hidden"}}></i>
+            <i class="fas fa-bolt"></i>
+            <i class="fas fa-tint"></i>
+            <i class="fas fa-balance-scale"></i>
+          </span>
+        </div>
+        <div class='number' style={{marginTop: "5px", opacity: 0.3, fontSize: "14px", fontWeight: 900}}><span style={{visibility: "hidden"}}>1</span> 2 3<br />
+      <span style={{visibility: "hidden"}}>4</span> 5</div>
+      </div>
+      <div class='card' style={{left: 0, top: 225}}>
       </div>
     </div>
   }
