c8bb21036e2e644f006b3a6f38071d0f576bae03
Xavier Shay <contact@xaviershay.com>
2019-02-18 17:08:19 -0800

    Show possible ranks/colors on back of cards

--- frontend/src/Board.js
+++ frontend/src/Board.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import * as d3array from 'd3-array'
 import * as d3 from 'd3'
 
+let allColors = ['red', 'green', 'yellow', 'blue', 'white']
+let allRanks = [1,2,3,4,5]
+
 function px(n) {
   return n + "px"
 }
@@ -21,7 +24,7 @@ function makeRange(n) {
 }
 
 function isFlipped(d) {
-  return d.rank === undefined
+  return d.rank === null || !(d.rank >= 0)
 }
 
 function createNumberCardFront(front, d) {
@@ -89,6 +92,35 @@ function createCardFront(d) {
   }
 }
 
+function createCardBack(card) {
+  let face = d3.select(this)
+
+  if (card.possibleRanks && card.possibleColors) {
+    face
+      .append('div')
+      .attr('class', 'possible-colors')
+      .selectAll('i')
+        .data(allColors)
+        .join(
+          enter => enter.append('i')
+            .attr('class', d => iconFor(d))
+            .style('opacity', d => card.possibleColors.indexOf(d) >= 0 ? 0.8 : 0.3)
+        )
+
+    face
+      .append('div')
+      .attr('class', 'possible-ranks')
+      .selectAll('span')
+        .data(allRanks)
+        .join(
+          enter => enter.append('span')
+            .text(d => d)
+            .style('opacity', d => card.possibleRanks.indexOf(d) >= 0 ? 0.8 : 0.3)
+        )
+
+  }
+}
+
 class Board extends Component {
   constructor(props){
     super(props)
@@ -113,8 +145,6 @@ class Board extends Component {
     let data =
       Array.from(d3array.group(rawData, d => d.location.join('-')).values())
         .flatMap(cs => cs.sort((x, y) => d3.ascending(x.rank, y.rank)).map((c, i) => Object.assign({}, c, {index: i})))
-
-    let allColors = ['red', 'green', 'yellow', 'blue', 'white']
 
     allColors.forEach(color => {
       data.push({id: '0-' + color, rank: 0, color: color, location: ['table']})
@@ -258,6 +288,7 @@ class Board extends Component {
                 .call(container => {
                   container.append('div')
                     .attr('class', 'face card--back')
+                    .each(createCardBack)
                   container.append('div')
                     .attr('class', 'face card--front')
                     .each(createCardFront)
